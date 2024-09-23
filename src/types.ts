c80a608879bdfc82c64dcc7e59db33a5b64a3d67
Yaoqx <1064245198@qq.com>
2024-09-23 22:23:37 +0800

    update

--- src/types.ts
+++ src/types.ts
@@ -1,51 +1,49 @@
 export interface Post {
-    data: {
-      [x: string]: any;
-      title: string;
-      image: string;
-      desc: string;
-      date: string;
-      badge: string;
-      categories: string[];
-      tags: string[];
-    };
-    slug: string;
-  }
-  
-  export interface Page {
-    url: {
-      prev?: string;
-      next?: string;
-    };
-    data: Post[];
-    total: number;
-    size: number;
-    current: string;
-  }
-  
-  export interface CardInfo {
+  data: {
+    [x: string]: any;
     title: string;
-    image?: string;
-    date?: string;
-    badge?: string;
-    categories?: string[];
-    tags?: string[];
-    word?: string;
-    time?: string;
-    isBlog: boolean;
-    url?: string;
-  }
-  
-  export interface EnvelopeInfo {
-    title: string;
+    image: string;
     desc: string;
-    image?: string;
-    date?: string;
-    badge?: string;
-    categories?: string[];
-    tags?: string[];
-    word?: string;
-    time?: string;
-    isBlog: boolean;
-    url: string;
-  }
+    date: string;
+    badge: string;
+    categories: string[];
+    tags: string[];
+  };
+  slug: string;
+}
+
+export interface Page {
+  url: {
+    prev?: string;
+    next?: string;
+  };
+  data: Post[];
+  total: number;
+  size: number;
+  current: string;
+}
+
+export interface CardInfo {
+  title: string;
+  image?: string;
+  date?: string;
+  badge?: string;
+  categories?: string[];
+  tags?: string[];
+  word?: string;
+  time?: string;
+  isBlog: boolean;
+  url?: string;
+}
+
+export interface EnvelopeInfo {
+  title: string;
+  desc: string;
+  image?: string;
+  date?: string;
+  badge?: string;
+  categories?: string[];
+  tags?: string[];
+  isBlog: boolean;
+  url: string;
+}
